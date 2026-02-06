4bae7bffd4b2f471c49e99db85f3f5907a8117fc
petrofdy <dylanpetroff13@gmail.com>
2026-02-06 10:05:16 -0500

    Started creating the hud

--- FinalPuzzleARG/Assets/Scripts/SwitchingSceneHandler.cs
+++ FinalPuzzleARG/Assets/Scripts/SwitchingSceneHandler.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class SwitchingSceneHandler : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -11,6 +11,6 @@ public class TestScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //hello world
+        
     }
 }
